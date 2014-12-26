63017f9594c1f5a191400cb16b71a0d2417fdba8
Chris Pierick <chris@twotoasters.com>
2014-12-26 14:36:10 -0500

    Added RecyclerCursorAdapter. I really did basicly just take the support library CursorAdapter. As such I state that and add all of the Licesning and Documentation for the original adapter.

---
+++ library/src/main/java/com/twotoasters/sectioncursoradapter/adapter/CursorFilter.java
@@ -0,0 +1,49 @@
+package com.twotoasters.sectioncursoradapter.adapter;
+
+import android.database.Cursor;
+import android.widget.Filter;
+
+class CursorFilter extends Filter {
+
+    CursorFilterClient mClient;
+
+    CursorFilter(CursorFilterClient client) {
+        mClient = client;
+    }
+
+    interface CursorFilterClient {
+        CharSequence convertToString(Cursor cursor);
+        Cursor runQueryOnBackgroundThread(CharSequence constraint);
+        Cursor getCursor();
+        void changeCursor(Cursor cursor);
+    }
+
+    @Override
+    public CharSequence convertResultToString(Object resultValue) {
+        return mClient.convertToString((Cursor) resultValue);
+    }
+
+    @Override
+    protected FilterResults performFiltering(CharSequence constraint) {
+        Cursor cursor = mClient.runQueryOnBackgroundThread(constraint);
+
+        FilterResults results = new FilterResults();
+        if (cursor != null) {
+            results.count = cursor.getCount();
+            results.values = cursor;
+        } else {
+            results.count = 0;
+            results.values = null;
+        }
+        return results;
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, FilterResults results) {
+        Cursor oldCursor = mClient.getCursor();
+
+        if (results.values != null && results.values != oldCursor) {
+            mClient.changeCursor((Cursor) results.values);
+        }
+    }
+}
